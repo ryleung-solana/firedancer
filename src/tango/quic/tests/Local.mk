597f1062fc4561470fe100cab3296359d816d18e
Charles Li <charles.dt.li@gmail.com>
2023-05-13 23:16:01 +0000

    feat(quic): retry packet encrypt decrypt

--- src/tango/quic/tests/Local.mk
+++ src/tango/quic/tests/Local.mk
@@ -17,6 +17,7 @@ $(call make-unit-test,test_quic_tls_decrypt,test_tls_decrypt,fd_quic fd_ballet f
 $(call make-unit-test,test_quic_tls_pcap,test_tls_pcap,fd_quic fd_ballet fd_util)
 $(call make-unit-test,test_quic_tls_pcap2,test_tls_pcap2,fd_quic fd_ballet fd_util)
 $(call make-unit-test,test_quic_tls_both,test_tls_quic_both,fd_quic fd_util)
+$(call make-unit-test,test_quic_retry,test_quic_retry,fd_aio fd_quic fd_ballet fd_tango fd_util)
 
 $(call run-unit-test,test_quic_hs)
 $(call run-unit-test,test_quic_streams)
